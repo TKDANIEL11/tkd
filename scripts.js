f4ae7dc2723c086361d0325b1d97f29faead70a5
TKDANIEL11 <DANIELTKD120@GMAIL.COM>
2024-09-18 21:09:45 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    alert("Thank you for contacting TKD! We will get back to you shortly.");
+});
